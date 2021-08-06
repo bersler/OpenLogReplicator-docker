60bb27c33d0c24c9bed1ebcbaa1ef0e72664ce70
Adam Leszczynski <aleszczynski@bersler.com>
2021-08-07 01:23:01 +0200

    parametrized docker files

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@
 # along with Open Log Replicator; see the file LICENSE.txt  If not see
 # <http://www.gnu.org/licenses/>.
 
-docker build -t bersler/openlogreplicator:centos-8 -f Dockerfile-centos-8 .
-docker build -t bersler/openlogreplicator-pb:centos-8 -f Dockerfile-centos-8-pb .
 docker build -t bersler/openlogreplicator:ubuntu-20.04 -f Dockerfile-ubuntu-20.04 .
+docker build -t bersler/openlogreplicator:centos-8 -f Dockerfile-centos-8 .
 docker build -t bersler/openlogreplicator-pb:ubuntu-20.04 -f Dockerfile-ubuntu-20.04-pb .
+docker build -t bersler/openlogreplicator-pb:centos-8 -f Dockerfile-centos-8-pb .
