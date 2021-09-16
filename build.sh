22bf1461da8c99abab311f6cdeb65c8d2ab40c6d
Adam Leszczynski <aleszczynski@bersler.com>
2021-09-17 00:56:45 +0200

    update to 0.9.27-beta

--- build.sh
+++ build.sh
@@ -22,9 +22,9 @@ USER=`whoami`
 GIDOLR=`id -r -g ${USER}`
 UIDOLR=`id -r -u ${USER}`
 GIDORA=1000
-docker build -t bersler/openlogreplicator:centos-8 -f Dockerfile-centos-8 --build-arg GIDOLR=${GIDOLR} --build-arg UIDOLR=${UIDOLR} --build-arg GIDORA=${GIDORA} .
-docker build -t bersler/openlogreplicator:debian-11.0 -f Dockerfile-debian-11.0 --build-arg GIDOLR=${GIDOLR} --build-arg UIDOLR=${UIDOLR} --build-arg GIDORA=${GIDORA} .
+#docker build -t bersler/openlogreplicator:centos-8 -f Dockerfile-centos-8 --build-arg GIDOLR=${GIDOLR} --build-arg UIDOLR=${UIDOLR} --build-arg GIDORA=${GIDORA} .
+#docker build -t bersler/openlogreplicator:debian-11.0 -f Dockerfile-debian-11.0 --build-arg GIDOLR=${GIDOLR} --build-arg UIDOLR=${UIDOLR} --build-arg GIDORA=${GIDORA} .
 docker build -t bersler/openlogreplicator:ubuntu-20.04 -f Dockerfile-ubuntu-20.04 --build-arg GIDOLR=${GIDOLR} --build-arg UIDOLR=${UIDOLR} --build-arg GIDORA=${GIDORA} .
-docker build -t bersler/openlogreplicator-pb:centos-8 -f Dockerfile-centos-8-pb --build-arg GIDOLR=${GIDOLR} --build-arg UIDOLR=${UIDOLR} --build-arg GIDORA=${GIDORA} .
-docker build -t bersler/openlogreplicator-pb:debian-11.0 -f Dockerfile-debian-11.0-pb --build-arg GIDOLR=${GIDOLR} --build-arg UIDOLR=${UIDOLR} --build-arg GIDORA=${GIDORA} .
-docker build -t bersler/openlogreplicator-pb:ubuntu-20.04 -f Dockerfile-ubuntu-20.04-pb --build-arg GIDOLR=${GIDOLR} --build-arg UIDOLR=${UIDOLR} --build-arg GIDORA=${GIDORA} .
+#docker build -t bersler/openlogreplicator-pb:centos-8 -f Dockerfile-centos-8-pb --build-arg GIDOLR=${GIDOLR} --build-arg UIDOLR=${UIDOLR} --build-arg GIDORA=${GIDORA} .
+#docker build -t bersler/openlogreplicator-pb:debian-11.0 -f Dockerfile-debian-11.0-pb --build-arg GIDOLR=${GIDOLR} --build-arg UIDOLR=${UIDOLR} --build-arg GIDORA=${GIDORA} .
+#docker build -t bersler/openlogreplicator-pb:ubuntu-20.04 -f Dockerfile-ubuntu-20.04-pb --build-arg GIDOLR=${GIDOLR} --build-arg UIDOLR=${UIDOLR} --build-arg GIDORA=${GIDORA} .
